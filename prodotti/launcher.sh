d41d1e2d3d143fd0cf1657844639b8489b2d6c75
Giorgio Bolzon <gbolzon@inogs.it>
2019-01-24 17:24:10 +0100

    Minor bug fix and adding rean case

--- prodotti/launcher.sh
+++ prodotti/launcher.sh
@@ -4,13 +4,18 @@ INPUTDIR=/gpfs/scratch/userexternal/gbolzon0/OPEN_BOUNDARY/TEST_05/wrkdir/MODEL/
 OUTPUTDIR=/gpfs/work/OGS18_PRACE_P_0/PROD_COPERNICUS/prodotti/MONTHLY
 MASKFILE=/gpfs/scratch/userexternal/gbolzon0/OPEN_BOUNDARY/TEST_05/wrkdir/MODEL/meshmask.nc
 
+INPUTDIR=/gpfs/scratch/userexternal/gbolzon0/REA/2017
+OUTPUTDIR=/gpfs/work/OGS18_PRACE_P_0/PROD_COPERNICUS/prodotti/REAN/
+MASKFILE=/gpfs/scratch/userexternal/gbolzon0/REA/meshmask.nc
+
 mkdir -p $OUTPUTDIR
 HERE=$PWD
 cd $INPUTDIR
 ls ave*N1p.nc | cut -c 5-21 > $HERE/timelist
 
 cd $HERE
-mpirun python prodotti_copernicus_rea.py -i $INPUTDIR -o $OUTPUTDIR -t timelist -m $MASKFILE
+mpirun -np 1 python prodotti_copernicus_rea.py -i $INPUTDIR -o $OUTPUTDIR -t timelist2 -m $MASKFILE
 
-mpirun -np 1 python prodotti_copernicus.py -i $INPUTDIR -o $OUTPUTDIR -t timelist -m $MASKFILE -d sm -b 20190115 -tr daily
+mpirun -np 1 python prodotti_copernicus.py -i $INPUTDIR -o $OUTPUTDIR -t timelist -m $MASKFILE -d sm -b 20190115 --tr daily
+
 mpirun -np 1 python prodotti_copernicus.py -i $INPUTDIR -o $OUTPUTDIR -t timelist -m $MASKFILE -d an -b 20190115 --tr monthly
