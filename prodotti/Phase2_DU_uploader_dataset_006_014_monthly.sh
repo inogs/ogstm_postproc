afeb4c25ac824e25ee324281225c66358f0cb628
Giorgio Bolzon <gbolzon@inogs.it>
2020-02-19 16:55:17 +0100

    Bug fix about previous commit

--- prodotti/Phase2_DU_uploader_dataset_006_014_monthly.sh
+++ prodotti/Phase2_DU_uploader_dataset_006_014_monthly.sh
@@ -82,7 +82,7 @@ for file in `ls ${PROD_DIR}/${FILES_TO_SEND} ` ; do
     yyyy=${basefile:0:4}
       mm=${basefile:4:2}
     remotedir=/${product}/${dataset}/$yyyy  #/$mm
-    remotefile="${basefile}"
+    remotefile="${yyyy}/${basefile}"
     md5s=`md5sum $file|awk '{print $1}'`
 	StarTime=`date --utc +%Y%m%dT%H%M%SZ`
 	EndTime=${StarTime}
