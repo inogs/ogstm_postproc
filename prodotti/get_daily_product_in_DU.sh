b1fd5edb64558ddaef60876b995d1d9c4d5b5558
Giorgio Bolzon <gbolzon@inogs.it>
2020-03-11 17:11:46 +0100

    Using nrt-dev service

--- prodotti/get_daily_product_in_DU.sh
+++ prodotti/get_daily_product_in_DU.sh
@@ -38,7 +38,7 @@ done
 function ncftp_check {
 filename=$1
 
-HOST=nrt.cmems-du.eu
+HOST=nrt-dev.cmems-du.eu
 ncftp -P 21 -u MED_OGS_TRIESTE_IT -p NEdifupa $HOST <<EOF
 dir $filename
 EOF
@@ -48,11 +48,11 @@ EOF
 PRODUCT=MEDSEA_ANALYSIS_FORECAST_BIO_006_014
 
 case $TYPE in 
-   "BIOL" ) dataset=med-ogs-bio-an-fc-d ;;
-   "CARB" ) dataset=med-ogs-car-an-fc-d ;;
-   "NUTR" ) dataset=med-ogs-nut-an-fc-d ;;
-   "PFTC" ) dataset=med-ogs-nut-an-fc-d ;;
-   "CO2F" ) dataset=med-ogs-co2-an-fc-d ;;
+   "BIOL" ) dataset=med00-ogs-bio-an-fc-d ;;
+   "CARB" ) dataset=med00-ogs-car-an-fc-d ;;
+   "NUTR" ) dataset=med00-ogs-nut-an-fc-d ;;
+   "PFTC" ) dataset=med00-ogs-nut-an-fc-d ;;
+   "CO2F" ) dataset=med00-ogs-co2-an-fc-d ;;
    * )  echo Wrong type ; usage; exit 1 ;;
 esac   
 
