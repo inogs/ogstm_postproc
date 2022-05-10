1c07c081f9bf65fc20bac14296617dc56c87571f
Giorgio Bolzon <gbolzon@inogs.it>
2022-05-10 17:00:00 +0200

    Replacing 1.e-8 with interpolated absorption in deep water

--- kd.py
+++ kd.py
@@ -58,6 +58,7 @@ freq_nanom = [250, 325, 350, 375, 400, 425, 450, 475, 500, 525, 550, 575, 600, 6
        650, 675, 700, 725, 775, 850, 950, 1050, 1150, 1250, 1350, 1450, 1550, 1650,
        1750, 1900, 2200, 2900, 3700]
 
+absorption=[0.6112, 0.0218, 0.0081, 0.0057, 0.0047,0.0049,0.0085,0.0117,0.0215]#morel
 
 
 def load_data(prefix,index_freq, dateobj):
@@ -80,24 +81,22 @@ def get_E(dateobj, interp_data):
     Es[~opt_mask] = 1.e+20
     return Ed + Es
 
+def freq_absorption(interp_data):
+    freq_before, freq_after, w = interp_data
+    return absorption[freq_before]*(1-w) + absorption[freq_after]*w
+
 TL = TimeList.fromfilenames(None, INPUTDIR, "ave*nc", filtervar="Ed_0500")
 
 for d in TL.Timelist:
     for wavelengh in [380, 412, 490]:
         interp_data = data_for_linear_interp(freq_nanom,wavelengh)
         E = get_E(d, interp_data)
-        KD = np.ones((jpk,jpj,jpi),np.float32)*1.e-08
+        KD = np.ones((jpk,jpj,jpi),np.float32) * freq_absorption(interp_data)
         jk_lim = TheMask.getDepthIndex(500.)
 
 
         KD[:jk_lim,:,:] = -np.log(E[1:jk_lim+1,:,:]/E[0:jk_lim,:,:])/TheMask.e3t[:jk_lim,:,:]
         KD[~TheMask.mask] = 1.e+20
-    # check
-        for k in range(10):
-            junk = KD[k,:,:]
-            max_val = junk[TheMask.mask[k,:,:]].max()
-            ii = KD[k,:,:] == max_val
-            print(k, np.nonzero(ii))
 
 
         varname="kd%s" %wavelengh
