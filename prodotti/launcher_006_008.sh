faf6096cccb3eb78e62dac1b830b97f20fc0761a
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2022-10-13 10:51:41 +0200

    Enabling launcher_006_008.sh for clim generation, now it's a job

--- prodotti/launcher_006_008.sh
+++ prodotti/launcher_006_008.sh
@@ -1,26 +1,47 @@
 #! /bin/bash
 
-INPUTDIR=/g100_scratch/userexternal/gbolzon0/V9C/PRODOTTI/AVE_CLIM
-OUTPUTDIR=/g100_scratch/userexternal/gbolzon0/V9C/PRODOTTI/
-MASKFILE=/gss/gss_work/DRES_OGS_BiGe/ateruzzi/RA_24/input/setup/PREPROC/MASK/gdept_3d/ogstm/meshmask.nc
+
+#SBATCH --job-name=POST
+#SBATCH -N1
+#SBATCH --ntasks-per-node=12
+#SBATCH --time=01:30:00
+#SBATCH --mem=100gb
+#SBATCH --account=OGS_devC
+#SBATCH --partition=g100_usr_prod
+#SBATCH --qos=g100_qos_dbg
+
+unset I_MPI_PMI_LIBRARY
+
+. ../profile.inc
+INPUTDIR=/g100_scratch/userexternal/gbolzon0/RA_24/AVE/MONTHLY
+OUTPUTDIR=/g100_scratch/userexternal/gbolzon0/RA_24/AVE/CLIM
+MASKFILE=/g100_scratch/userexternal/gbolzon0/RA_24/meshmask.nc
+
+for var in N1p N3n N4n P_c P_l O2o ppn pH O3c O3h CO2airflux pCO2 ; do
+ my_prex_or_die "mpirun -np 12 python clim_generator.py -i $INPUTDIR -o $OUTPUTDIR -m $MASKFILE -v $var"
+done
 
 
-python prodotti_copernicus_rea_clim.py -i $INPUTDIR -o $OUTPUTDIR -m $MASKFILE -b 20220607 --bulltype analysis
+INPUTDIR=/g100_scratch/userexternal/gbolzon0/RA_24/AVE/CLIM
+OUTPUTDIR=/g100_scratch/userexternal/gbolzon0/RA_24/PRODUCTS/CLIM
 
+my_prex_or_die "python prodotti_copernicus_rea_clim.py -i $INPUTDIR -o $OUTPUTDIR -m $MASKFILE -b 20221013 --bulltype analysis"
 
+exit 0
 
-INPUTDIR=/g100_scratch/userexternal/gbolzon0/V9C/PRODOTTI/AVE_YEARLY
-OUTPUTDIR=/g100_scratch/userexternal/gbolzon0/V9C/PRODOTTI/YEARLY
+INPUTDIR=/g100_scratch/userexternal/gbolzon0/RA_24/AVE/YEARLY
+OUTPUTDIR=/g100_scratch/userexternal/gbolzon0/RA_24/PRODUCTS/YEARLY
 
 mkdir -p $OUTPUTDIR
 HERE=$PWD
 cd $INPUTDIR
-ls ave.*N1p.nc | cut -c 5-12 > $HERE/timelist
+ls ave.*N1p.nc | cut -c 5-21  > $HERE/timelist
 
 cd $HERE
 
-mpirun -np 1 python prodotti_copernicus_rea.py -i $INPUTDIR -o $OUTPUTDIR -t timelist -m $MASKFILE -b 20110607 --tr yearly --bulltype analysis
+mpirun -np 1 python prodotti_copernicus_rea.py -i $INPUTDIR -o $OUTPUTDIR -t timelist -m $MASKFILE -b 20221004 --tr yearly --bulltype analysis
 
+exit 0
 
 BASEDIR=/gpfs/scratch/userexternal/gbolzon0/REA_24/TEST_22/wrkdir/MODEL/
 INPUTDIR=$BASEDIR/AVE_FREQ_1/
