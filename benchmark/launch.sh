ab67adc6165bfa78906a6904883c48f65772b3fb
Giorgio Bolzon <gbolzon@inogs.it>
2023-07-14 15:34:20 +0200

    Restructuring launch.sh to make it more user friendly

--- benchmark/launch.sh
+++ benchmark/launch.sh
@@ -9,14 +9,26 @@ module load netcdff/4.5.3--oneapi--2021.2.0-ifort
 source /g100_work/OGS21_PRACE_P/COPERNICUS/py_env_3.6.8/bin/activate
 
 
+#Hypotheses:
+# 1. data are in $CINECA_SCRATCH (model and postproc)
+# 2. $OPA_HOME is path relative to $CINECA_SCRATCH
+
+
 #  user settings #########################
-export CINECA_WORK=/g100_work/OGS_devC
-export OPA_HOME=Benchmark/HC
+export OPA_HOME=Benchmark/DA_SAT
+#
+export VALIDATION_DIR=/g100_work/OGS_devC/Benchmark/pub/Benchmark/DA_SAT  ## the path after pub/ will be published https://medeaf.inogs.it/internal-validation
+#
 # EDIT spaghetti_plot_user_setting.txt
+#
+export CINECA_WORK=/g100_work/OGS_devC
+POSTPROCDIR=$CINECA_WORK/$OPA_HOME/wrkdir/POSTPROC  ## $CINECA_WORK or $CINECA_SCRATCH
 ##########################################
 
 
-BITSEA=$CINECA_WORK/$OPA_HOME/wrkdir/POSTPROC/bit.sea
+
+
+BITSEA=${POSTPROCDIR}/bit.sea
 export PYTHONPATH=:$BITSEA
 HERE=$PWD
 
@@ -28,26 +40,22 @@ INPUTDIR=$CINECA_SCRATCH/$OPA_HOME/wrkdir/MODEL/AVE_FREQ_1/
  BASEDIR=$CINECA_SCRATCH/$OPA_HOME/wrkdir/POSTPROC/output/PROFILATORE/
 PROFILES=$CINECA_SCRATCH/$OPA_HOME/wrkdir/POSTPROC/output/AVE_FREQ_2/STAT_PROFILES
 
-sed -e "s%\@\@INPUTDIR\@\@%${INPUTDIR}%g" -e "s%\@\@BASEDIR\@\@%${BASEDIR}%g " profiler.tpl > $HERE/profiler.py
-sed -e "s%actual%${PROFILES}%g " timeseries_user_settings.txt > $HERE/profiles_plotter_user_settings.txt
+cd $POSTPROCDIR
 
-cp $HERE/profiles_plotter_user_settings.txt $BITSEA/validation/deliverables
-
-cd $CINECA_WORK/$OPA_HOME/wrkdir/POSTPROC
 
 if [[ -d bit.sea ]] ; then
     cd $HERE
 else
     git clone git@github.com:inogs/bit.sea.git
     cd $BITSEA/validation/deliverables
-    cp $HERE/profiler.py .
+    sed -e "s%\@\@INPUTDIR\@\@%${INPUTDIR}%g" -e "s%\@\@BASEDIR\@\@%${BASEDIR}%g " $HERE/profiler.tpl > profiler.py
+    sed -e "s%actual%${PROFILES}%g " $HERE/timeseries_user_settings.txt > profiles_plotter_user_settings.txt
     python profiler.py
 fi
 
 cd $HERE
-export VALIDATION_DIR=$CINECA_WORK/$OPA_HOME/wrkdir/POSTPROC/output/VALIDATION
-export VALIDATION_DIR=/g100_work/OGS_devC/Benchmark/pub/HC
 
-# 1. sbatch job.POST.slurm.galileo
-# 2. sbatch job.serial.slurm
+
+# 1. sbatch job.POST.slurm.galileo # 1h
+# 2. sbatch job.serial.slurm       # 1h30
 
