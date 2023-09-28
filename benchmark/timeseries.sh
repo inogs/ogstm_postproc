5f9515d26e4770fb07e55d3f075857a7dcf7d3d4
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2023-09-28 15:36:52 +0200

    moving optical variables defined in VarDescriptor_2.xml in TIMESERIES/optics/

--- benchmark/timeseries.sh
+++ benchmark/timeseries.sh
@@ -11,3 +11,20 @@ cd $BITSEA/validation/deliverables
 
 
 my_prex_or_die "python profiles_plotter.py -o $VALIDATION_DIR/TIMESERIES -m $MASKFILE -f profiles_plotter_user_settings.txt"
+
+# moving optical variables in TIMESERIES/optics/
+mkdir -p $VALIDATION_DIR/TIMESERIES/optics
+VARLIST="
+        theta_chl
+	theta_P1
+	theta_P2
+	theta_P3
+	theta_P4
+	limPAR_P1
+	limPAR_P2
+	limPAR_P3
+	limPAR_P4"
+
+for var in $VARLIST ; do
+     [[ -d $VALIDATION_DIR/TIMESERIES/${var} ]] && mv $VALIDATION_DIR/TIMESERIES/${var}/ $VALIDATION_DIR/TIMESERIES/optics/
+done
