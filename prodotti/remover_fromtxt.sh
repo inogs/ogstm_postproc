2f6c5ae258fd36de2397419d64f135d6dd70eb95
Giorgio Bolzon <gbolzon@dhcp-sissa-180.ogs.it>
2024-02-13 14:22:12 +0100

    Improving remover_fromtxt.sh

--- prodotti/remover_fromtxt.sh
+++ prodotti/remover_fromtxt.sh
@@ -17,7 +17,7 @@ cmems_mod_med_bgc-optics_anfc_4.2km_P1D-m
 for dataset in $DATASETS ; do  
 
    filename=$PWD/${dataset}.txt
-   echo n | copernicusmarine get -i $dataset --filter "*202312*d-OGS*" --show-outputnames | grep fc-sv08 | grep INFO > $filename 
+   echo n | copernicusmarine get -i $dataset --filter "*/202312*d-OGS*" --show-outputnames --disable-progress-bar | grep fc-sv08 | grep INFO > $filename 
    python remover_fromtxt.py -i $filename -o $OUTDIR
 
 done
