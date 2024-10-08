e601152aa97de6a5bcb00ff72dcb673046e9ec76
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2024-10-08 10:44:43 +0200

    Generation MDS-DV dirs for EIS 202411, and sample products

--- prodotti/launcher.sh
+++ prodotti/launcher.sh
@@ -1,18 +1,28 @@
 #! /bin/bash
 
-INPUTDIR=/gpfs/scratch/userexternal/gbolzon0/OPEN_BOUNDARY/TEST_07/wrkdir/MODEL/AVE_FREQ_1
-OUTPUTDIR=/gpfs/scratch/userexternal/gbolzon0/OPEN_BOUNDARY/TEST_07/wrkdir/POSTPROC/output/AVE_FREQ_1/PROD
-MASKFILE=/gpfs/scratch/userexternal/gbolzon0/OPEN_BOUNDARY/TEST_07/wrkdir/MODEL/meshmask.nc
+INPUTDIR=/g100_work/OGS_prodC/OPA/V10C-prod/archive/analysis/20241001/POSTPROC/AVE_FREQ_1/ARCHIVE/
+OUTPUTDIR=/g100_scratch/userexternal/gbolzon0/V11C/test_upload/DAILY
+MASKFILE=/g100_work/OGS_prodC/OPA/V10C-prod/etc/static-data/MED24_125/meshmask.nc
 
 mkdir -p $OUTPUTDIR
 HERE=$PWD
 cd $INPUTDIR
-ls ave.20190101*N1p.nc | cut -c 5-12 > $HERE/timelist
-python tuesday.py -i $INPUTDIR
+ls ave.*N1p.nc | cut -c 5-12 > $HERE/timelist
 
 cd $HERE
 
 
-mpirun -np 1 python prodotti_copernicus.py -i $INPUTDIR -o $OUTPUTDIR -t timelist -m $MASKFILE -d sm -b 20190115 --tr daily
+mpirun -np 1 python prodotti_copernicus.py -i $INPUTDIR -o $OUTPUTDIR -t timelist -m $MASKFILE -d an -b 20241008 --tr daily
 
+### monthly section
 
+INPUTDIR=/g100_scratch/userexternal/gbolzon0/V11C/test_upload/ogstm_postproc/prodotti/MONTHLY
+OUTPUTDIR=/g100_scratch/userexternal/gbolzon0/V11C/test_upload/MONTHLY
+mkdir -p $OUTPUTDIR
+HERE=$PWD
+cd $INPUTDIR
+ls ave.*N1p.nc | cut -c 5-12 > $HERE/timelist
+
+cd $HERE
+
+mpirun -np 1 python prodotti_copernicus.py -i $INPUTDIR -o $OUTPUTDIR -t timelist -m $MASKFILE -d an -b 20241008 --tr monthly
