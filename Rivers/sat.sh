2c7c3b9ce26216eaa6fbf561257a2a951cafcef4
Giorgio Bolzon <gbolzon@ogs.it>
2025-02-17 16:07:19 +0100

    Bug fix, adding -z rivers in sat.sh

--- Rivers/sat.sh
+++ Rivers/sat.sh
@@ -28,8 +28,8 @@ for VAR in   P_l  P1l    P2l    P3l    P4l kd490 RRS412    RRS443    RRS490    R
    my_prex_or_die "mkdir -p $VALIDATION_DIR/SAT/Fig4.3_BiasRmsd/Rivers/${VAR}"
 
  
-   my_prex_or_die "python plot_timeseries_STD.py      -v $VAR -i $SAT_VALID_DIR/RIVERS -o $VALIDATION_DIR/SAT/Fig4.2_Timeseries/Rivers/${VAR} -c everywhere $PERIOD"
-   my_prex_or_die "python plot_timeseries_RMS_CORR.py -v $VAR -i $SAT_VALID_DIR/RIVERS -o $VALIDATION_DIR/SAT/Fig4.3_BiasRmsd/Rivers/${VAR}   -c everywhere $PERIOD" # table4.1
+   my_prex_or_die "python plot_timeseries_STD.py      -v $VAR -i $SAT_VALID_DIR/RIVERS -o $VALIDATION_DIR/SAT/Fig4.2_Timeseries/Rivers/${VAR} -c everywhere $PERIOD -z rivers"
+   my_prex_or_die "python plot_timeseries_RMS_CORR.py -v $VAR -i $SAT_VALID_DIR/RIVERS -o $VALIDATION_DIR/SAT/Fig4.3_BiasRmsd/Rivers/${VAR}   -c everywhere $PERIOD -z rivers" # table4.1
 
 done
 
