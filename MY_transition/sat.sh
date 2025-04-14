3c30e254f878bddf6e2b33e4d96f79ecc844a577
Laura Feudale <lfeudale@inogs.it>
2025-04-14 16:14:08 +0200

    Change of the statring time and ending time.

--- MY_transition/sat.sh
+++ MY_transition/sat.sh
@@ -18,13 +18,13 @@ for VAR in   P_l  P1l    P2l    P3l    P4l ; do
 
    cd $BITSEA/src/bitsea/validation/deliverables/
 
-   my_prex_or_die "python plot_timeseries_STD.py -v $VAR -i $SAT_VALID_DIR -o $VALIDATION_DIR/SAT/Fig4.2_Timeseries/offshore/${VAR} -c open_sea -s 20210101 -e 20250101"
-   my_prex_or_die "python plot_timeseries_RMS_CORR.py -v $VAR -i $SAT_VALID_DIR -c open_sea -o $VALIDATION_DIR/SAT/Fig4.3_BiasRmsd/offshore/${VAR} -s 20210101 -e 20250101" # table4.1
-   my_prex_or_die "python plot_timeseries_RMS_CORR.py -v $VAR -i $SAT_VALID_DIR -c coast -o $VALIDATION_DIR/SAT/Fig4.3_BiasRmsd/coast/${VAR} -s 20210101 -e 20250101"  # table4.2
+   my_prex_or_die "python plot_timeseries_STD.py -v $VAR -i $SAT_VALID_DIR -o $VALIDATION_DIR/SAT/Fig4.2_Timeseries/offshore/${VAR} -c open_sea -s 20220101 -e 20250101"
+   my_prex_or_die "python plot_timeseries_RMS_CORR.py -v $VAR -i $SAT_VALID_DIR -c open_sea -o $VALIDATION_DIR/SAT/Fig4.3_BiasRmsd/offshore/${VAR} -s 20220101 -e 20250101" # table4.1
+   my_prex_or_die "python plot_timeseries_RMS_CORR.py -v $VAR -i $SAT_VALID_DIR -c coast -o $VALIDATION_DIR/SAT/Fig4.3_BiasRmsd/coast/${VAR} -s 20220101 -e 20250101"  # table4.2
 
    my_prex_or_die "cp $VALIDATION_DIR/SAT/Fig4.3_BiasRmsd/offshore/${VAR}/table4.1_${VAR}.txt $VALIDATION_DIR/SAT/table4.1_offshore "
    my_prex_or_die "cp $VALIDATION_DIR/SAT/Fig4.3_BiasRmsd/coast/${VAR}/table4.1_${VAR}.txt    $VALIDATION_DIR/SAT/table4.2_coast/table4.2_${VAR}.txt "
 
 done
 
-my_prex_or_die "python plot_pfts.py -i $SAT_VALID_DIR -o $VALIDATION_DIR/SAT/pfts/ -s 20210101 -e 20250101"
+my_prex_or_die "python plot_pfts.py -i $SAT_VALID_DIR -o $VALIDATION_DIR/SAT/pfts/ -s 20220101 -e 20250101"
