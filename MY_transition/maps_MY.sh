c7b79e9b12ac06a490dc0287e2b84d4f45dc7b53
Giorgio Bolzon <gbolzon@ogs.it>
2025-05-02 16:02:40 +0200

    Renaming variables

--- MY_transition/maps_MY.sh
+++ MY_transition/maps_MY.sh
@@ -12,8 +12,8 @@ fi
 
 for I in 1 2; do
    case $1 in
-      "-s" ) YEAR=$2;;
-      "-e" ) YEAR_E=$2;;
+      "-s" ) YEAR1=$2;;
+      "-e" ) YEAR2=$2;;
         *  ) echo "Unrecognized option $1." ; usage;  exit 1;;
    esac
    shift 2
@@ -21,7 +21,7 @@ done
 
 
 . ../profile.inc
-. ./config.sh -y $YEAR
+. ./config.sh -y $YEAR1
 
 INPUTDIR=$CINECA_SCRATCH/$OPA_HOME/wrkdir/POSTPROC/output/YEARLY
 
@@ -34,18 +34,18 @@ mkdir -p $VALIDATION_DIR/MAPS/Z_c/
 
 cd $BITSEA/src/bitsea/validation/deliverables
 
-COMMONS_PARAMS="-m $MASKFILE  -l Plotlist_bio.xml -s ${YEAR}0101 -e ${YEAR_E}1231"
+COMMONS_PARAMS="-m $MASKFILE  -l Plotlist_bio.xml -s ${YEAR1}0101 -e ${YEAR2}1231"
 
 my_prex_or_die "python averager_and_plot_map.py -i $INPUTDIR  -v P_l  -t mean -o $VALIDATION_DIR/MAPS/P_l $COMMONS_PARAMS "     # Fig4.1 CHL-LAYER-Y-CLASS1-[CLIM/LIT]-MEAN
-my_prex_or_die "python averager_and_plot_map.py -i $INPUTDIR  -v P_c  -t mean -m $MASKFILE -o  $VALIDATION_DIR/MAPS/P_c  -l Plotlist_bio_Int.xml -s ${YEAR}0101 -e ${YEAR_E}1231"
+my_prex_or_die "python averager_and_plot_map.py -i $INPUTDIR  -v P_c  -t mean -m $MASKFILE -o  $VALIDATION_DIR/MAPS/P_c  -l Plotlist_bio_Int.xml -s ${YEAR1}0101 -e ${YEAR2}1231"
 my_prex_or_die "python averager_and_plot_map.py -i $INPUTDIR  -v Z_c  -t integral -o  $VALIDATION_DIR/MAPS/Z_c   $COMMONS_PARAMS "
 
 my_prex_or_die "python averager_and_plot_map_ppn.py -i $INPUTDIR  -v ppn  -t integral -o $VALIDATION_DIR/MAPS/ppn $COMMONS_PARAMS "
 
 
 #CHL-LAYER-Y-CLASS1-[CLIM/LIT]-MEAN from SATELLITE:
-my_prex_or_die "python sat_ave_and_plot.py -i $SAT_CHLWEEKLY_DIR -m $MASKFILE  -o  $VALIDATION_DIR/MAPS/P_l/MY   -s ${YEAR}0101 -e ${YEAR_E}1231 "
+my_prex_or_die "python sat_ave_and_plot.py -i $SAT_CHLWEEKLY_DIR -m $MASKFILE  -o  $VALIDATION_DIR/MAPS/P_l/MY   -s ${YEAR1}0101 -e ${YEAR2}1231 "
 
 #INPUTDIR=$CINECA_SCRATCH/$OPA_HOME/wrkdir/MODEL/AVE_FREQ_2/
 #INPUTDIR=
-my_prex_or_die "python sat_model_RMSD_and_plot.py -s $SAT_CHLWEEKLY_DIR -i $INPUTDIR -m $MASKFILE -o $VALIDATION_DIR/MAPS/P_l/MY -st ${YEAR}0101 -e ${YEAR_E}1231"
+my_prex_or_die "python sat_model_RMSD_and_plot.py -s $SAT_CHLWEEKLY_DIR -i $INPUTDIR -m $MASKFILE -o $VALIDATION_DIR/MAPS/P_l/MY -st ${YEAR1}0101 -e ${YEAR2}1231"
