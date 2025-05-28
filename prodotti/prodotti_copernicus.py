29d235b41f376853dc6770a13d7d403cc195a358
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2025-05-28 16:34:20 +0200

    Setting V12C product nomenclature (sv10)

--- prodotti/prodotti_copernicus.py
+++ prodotti/prodotti_copernicus.py
@@ -178,15 +178,13 @@ def create_Structure(filename, fgroup):
     return ncOUT
 
 
-def V6_filename(timeobj,FGroup):
-    return timeobj.strftime('%Y%m%d_') + tr + "-OGS--" + FGroup + "-MedBFM3-MED-b" + bulletin_date +"_" + DType + "-sv06.00.nc"
-def V9_filename(timeobj,FGroup):
-    return timeobj.strftime('%Y%m%d_') + tr + "-OGS--" + FGroup + "-MedBFM4-MED-b" + bulletin_date +"_" + DType + "-sv09.00.nc"
+def set_filename(timeobj,FGroup):
+    return timeobj.strftime('%Y%m%d_') + tr + "-OGS--" + FGroup + "-MedBFM4-MED-b" + bulletin_date +"_" + DType + "-sv10.00.nc"
 
 for timestr in TIMELIST[rank::nranks]:
     timeobj = datetime.datetime.strptime(timestr,"%Y%m%d")
     for FGroup in FGROUPS:
-        product_file = V9_filename(timeobj, FGroup)
+        product_file = set_filename(timeobj, FGroup)
         print("rank =", rank, product_file, flush=True)
         ncOUT = create_Structure(OUTPUTDIR + product_file,FGroup)
         
