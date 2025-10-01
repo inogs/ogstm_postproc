358b0dec06a6a6b1aa11c5bac8fe3fec09dd75b6
Giorgio Bolzon <gbolzon@ogs.it>
2025-10-01 15:50:12 +0200

    renaming ncftp_command.sh -> EIS_prepare_dirs_006_014.sh and adding relative for 006_008

---
+++ prodotti/EIS_prepare_dirs_006_008.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+# Executed as the DV is opened by the CMEMS system to create the directory structure on the FTP server
+# before an Entry In Service (here EIS 202511)
+
+HOST=ftp-my.marine.copernicus.eu
+PRODUCT=MEDSEA_MULTIYEAR_BGC_006_008
+
+# daily section
+
+for YEAR in $(seq 2023 2028) ; do
+	for group in bio car nut co2 plankton; do
+	    dataset=cmems_mod_med_bgc-${group}_my_4.2km_P1D-m_202511
+	    ncftp -P 21 -u cmems_med_ogs -p 9J2e+uLU $HOST <<EOF
+cd /${PRODUCT}/${dataset}
+mkdir ${YEAR}
+cd ${YEAR}
+mkdir 01 02 03 04 05 06 07 08 09 10 11 12
+quit
+EOF
+
+    done
+done
+
+# monthly section
+
+for YEAR in $(seq 2023 2028) ; do
+        for group in  bio car nut co2 plankton; do
+            dataset=cmems_mod_med_bgc-${group}_my_4.2km_P1M-m_202511
+            ncftp -P 21 -u cmems_med_ogs -p 9J2e+uLU $HOST <<EOF
+cd /${PRODUCT}/${dataset}
+mkdir ${YEAR}
+quit
+EOF
+
+    done
+done
