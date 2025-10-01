b38196f89387fa1be6b610c78066ffa1a8b0ca91
Giorgio Bolzon <gbolzon@ogs.it>
2025-10-01 15:38:51 +0200

    Updating to EIS 202511

--- prodotti/ncftp_command.sh
+++ prodotti/ncftp_command.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
+# Executed as the DV is opened by the CMEMS system to create the directory structure on the FTP server
+# before an Entry In Service (here EIS 202511)
 
 HOST=ftp-nrt.marine.copernicus.eu
 PRODUCT=MEDSEA_ANALYSISFORECAST_BGC_006_014
 
 # daily section
 
-for YEAR in $(seq 2022 2028) ; do
+for YEAR in $(seq 2023 2029) ; do
 	for group in bio car nut co2 optics pft; do
-	    dataset=cmems_mod_med_bgc-${group}_anfc_4.2km_P1D-m_202411
-	    /g100_work/OGS23_PRACE_IT/COPERNICUS/bin/ncftp -P 21 -u cmems_med_ogs -p 9J2e+uLU $HOST <<EOF
+	    dataset=cmems_mod_med_bgc-${group}_anfc_4.2km_P1D-m_202511
+	    ncftp -P 21 -u cmems_med_ogs -p 9J2e+uLU $HOST <<EOF
 cd /${PRODUCT}/${dataset}
 mkdir ${YEAR}
 cd ${YEAR}
@@ -21,10 +23,10 @@ done
 
 # monthly section
 
-for YEAR in $(seq 2022 2028) ; do
+for YEAR in $(seq 2023 2029) ; do
         for group in bio car nut co2 optics pft; do
-            dataset=cmems_mod_med_bgc-${group}_anfc_4.2km_P1M-m_202411
-            /g100_work/OGS23_PRACE_IT/COPERNICUS/bin/ncftp -P 21 -u cmems_med_ogs -p 9J2e+uLU $HOST <<EOF
+            dataset=cmems_mod_med_bgc-${group}_anfc_4.2km_P1M-m_202511
+            ncftp -P 21 -u cmems_med_ogs -p 9J2e+uLU $HOST <<EOF
 cd /${PRODUCT}/${dataset}
 mkdir ${YEAR}
 quit
