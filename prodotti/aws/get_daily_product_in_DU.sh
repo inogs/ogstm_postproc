747a73f966618ac1faa4a0909b5cfe91f4ff9438
Giorgio Bolzon <gbolzon@ogs.it>
2025-11-18 15:00:30 +0100

    Setting EIS 202511 datasets

--- prodotti/aws/get_daily_product_in_DU.sh
+++ prodotti/aws/get_daily_product_in_DU.sh
@@ -39,12 +39,12 @@ done
 PRODUCT=MEDSEA_ANALYSISFORECAST_BGC_006_014
 
 case $TYPE in 
-   "BIOL" ) dataset=cmems_mod_med_bgc-bio_anfc_4.2km_P1D-m_202211 ;;
-   "CARB" ) dataset=cmems_mod_med_bgc-car_anfc_4.2km_P1D-m_202211 ;;
-   "NUTR" ) dataset=cmems_mod_med_bgc-nut_anfc_4.2km_P1D-m_202211 ;;
-   "PFTC" ) dataset=cmems_mod_med_bgc-pft_anfc_4.2km_P1D-m_202311 ;;
-   "CO2F" ) dataset=cmems_mod_med_bgc-co2_anfc_4.2km_P1D-m_202211 ;;
-   "EXCO" ) dataset=cmems_mod_med_bgc-optics_anfc_4.2km_P1D-m_202211 ;;
+   "BIOL" ) dataset=cmems_mod_med_bgc-bio_anfc_4.2km_P1D-m_202511 ;;
+   "CARB" ) dataset=cmems_mod_med_bgc-car_anfc_4.2km_P1D-m_202511 ;;
+   "NUTR" ) dataset=cmems_mod_med_bgc-nut_anfc_4.2km_P1D-m_202511 ;;
+   "PFTC" ) dataset=cmems_mod_med_bgc-pft_anfc_4.2km_P1D-m_202511 ;;
+   "CO2F" ) dataset=cmems_mod_med_bgc-co2_anfc_4.2km_P1D-m_202511 ;;
+   "EXCO" ) dataset=cmems_mod_med_bgc-optics_anfc_4.2km_P1D-m_202511 ;;
    * )  echo Wrong type ; usage; exit 1 ;;
 esac   
 
