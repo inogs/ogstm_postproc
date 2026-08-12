54060eb438e72f40b4c9ae807931be127eb74885
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2026-08-12 18:21:27 +0200

    maskload reads new polygon file

--- maskload.py
+++ maskload.py
@@ -4,13 +4,15 @@ from bitsea.commons.mask import Mask
 from bitsea.commons.submask import SubMask
 basins=os.getenv("BASINS","V2")
 
+
 if basins == 'V2':
     from bitsea.basins import V2 as OGS
 elif basins == "RIVERS":
     from bitsea.basins import RiverBoxes as OGS
 elif basins == "COASTAL12NM":
     from bitsea.basins import COASTAL12nm as OGS
-
+elif basins == "Atl":
+    from bitsea.basins import Atl as OGS
 
 annaCoast = False
 
